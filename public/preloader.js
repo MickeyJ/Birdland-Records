32a753eea116fe0c19572bdd553bb4b8cf17ac2d
MickeyJ <develop.agility@gmail.com>
2016-09-18 12:44:05 -0600

    calculating load time for window

--- public/preloader.js
+++ public/preloader.js
@@ -1,6 +1,14 @@
 
+var timerStartLoad = Date.now();
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+  console.log("Time until DOMready: ", Date.now()-timerStartLoad)
+});
 
 window.addEventListener('load', (e) =>{
+
+  console.log("Time until everything loaded: ", Date.now()-timerStartLoad);
 
   const britt = new Image();
   const robyn = new Image();
@@ -9,24 +17,35 @@ window.addEventListener('load', (e) =>{
   const dogwood = new Image();
   const fairylights = new Image();
 
-  britt.src = 'images/artists/BRITT_1.jpg';
-  robyn.src = 'images/artists/ROBYN_1.jpg';
-  elise.src = 'images/artists/ELISE_2.jpg';
-  cages.src = 'images/artists/CAGES_1.jpg';
-  dogwood.src = 'images/artists/DOGWOOD_1.jpg';
-  fairylights.src = 'images/artists/FAIRYLIGHTS_2.jpg';
-
   const artists = [britt, robyn, elise, cages, dogwood, fairylights];
-
   const imageContainers = document.querySelectorAll('.image-container');
   const artistNames = document.querySelectorAll('.artist-name');
   const overlays = document.querySelectorAll('.artist-link-overlay');
 
-  for (var i = 0; i < imageContainers.length; i++) {
-    imageContainers[i].appendChild(artists[i]);
-  }
+  britt.src = 'images/artists/BRITT_1.jpg';
+  britt.alt = 'BRITT MARGIT';
+
+  robyn.src = 'images/artists/ROBYN_1.jpg';
+  robyn.alt = 'ROBYN SHERWELL';
+
+  elise.src = 'images/artists/ELISE_2.jpg';
+  elise.alt = 'ELISE LEGENDRE';
+
+  cages.src = 'images/artists/CAGES_1.jpg';
+  cages.alt = 'CAGES OF LIGHT';
+
+  dogwood.src = 'images/artists/DOGWOOD_1.jpg';
+  dogwood.alt = 'DOGWOOD HEART';
+
+  fairylights.src = 'images/artists/FAIRYLIGHTS_2.jpg';
+  fairylights.alt = 'THE FAIRYLIGHTS';
+
+  imageContainers.forEach((x, i) =>{
+    x.appendChild(artists[i])
+  });
 
   setTimeout(() =>{
+
     artistNames.forEach(x =>{
       x.style.visibility = 'visible'
     });
@@ -34,8 +53,7 @@ window.addEventListener('load', (e) =>{
     overlays.forEach(x =>{
       x.style.visibility = 'visible'
     });
-  },100);
 
-
+  }, (Date.now()-timerStartLoad) * 2);
 
 });
