1b5c12dfc4cbb5952aaea2b880e0f7068915e308
MickeyJ <develop.agility@gmail.com>
2016-09-18 16:27:23 -0600

    fixed svg image quality with png

--- views/components/SiteHeader.jsx
+++ views/components/SiteHeader.jsx
@@ -8,7 +8,13 @@ const SiteHeader = (props) => (
     <div id="birdland-logo-container">
       <a href="/" >
         <svg viewBox={`0 0 ${svgSize} ${svgSize}`}>
-          <image xlinkHref="images/BL_logo3.svg" x="0" y="0" height={`${svgSize}px`} width={`${svgSize}px`}/>
+          <filter id="dropShadow">
+            <feOffset result = "offOut" in = "SourceGraphic" dx = "0" dy = "2"/>
+            <feColorMatrix result = "matrixOut" in = "offOut" type = "matrix" values = "0.2 0 0 0 0 0 0.2 0 0 0 0 0 0.2 0 0 0 0 0 1 0"/>
+            <feGaussianBlur result = "blurOut" in = "matrixOut" stdDeviation = "2"/>
+            <feBlend in = "SourceGraphic" in2 = "blurOut" mode = "normal"/>
+          </filter>
+          <image filter="url(#dropShadow)" xlinkHref="images/BL_logo3.png" x="0" y="0" height={`${svgSize}px`} width={`${svgSize}px`}/>
         </svg>
       </a>
     </div>
