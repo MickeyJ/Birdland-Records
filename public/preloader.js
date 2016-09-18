aa33be165b666d19f475808634fb42b701cef605
MickeyJ <develop.agility@gmail.com>
2016-09-18 15:24:14 -0600

    refactored preload to es5

--- public/preloader.js
+++ public/preloader.js
@@ -1,45 +1,59 @@
 
 var timerStartLoad = Date.now();
 
-document.addEventListener('DOMContentLoaded', () =>{
-  console.log("Time until DOMready: ", Date.now()-timerStartLoad)
+var DOMReadyTime,
+    windowLoadTime;
+
+var imageContainers,
+    artistNames,
+    overlays;
+
+var britt = new Image();
+var robyn = new Image();
+var elise = new Image();
+var cages = new Image();
+var dogwood = new Image();
+var fairylights = new Image();
+
+var artists = [ britt, robyn, elise, cages, dogwood, fairylights ];
+
+britt.src = 'images/artists/BRITT_1.jpg';
+britt.alt = 'BRITT MARGIT';
+
+robyn.src = 'images/artists/ROBYN_1.jpg';
+robyn.alt = 'ROBYN SHERWELL';
+
+elise.src = 'images/artists/ELISE_2.jpg';
+elise.alt = 'ELISE LEGENDRE';
+
+cages.src = 'images/artists/CAGES_1.jpg';
+cages.alt = 'CAGES OF LIGHT';
+
+dogwood.src = 'images/artists/DOGWOOD_1.jpg';
+dogwood.alt = 'DOGWOOD HEART';
+
+fairylights.src = 'images/artists/FAIRYLIGHTS_2.jpg';
+fairylights.alt = 'THE FAIRYLIGHTS';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  DOMReadyTime = Date.now()-timerStartLoad;
+
+  console.log("DOM Ready Time: ", Date.now()-timerStartLoad);
+
+  imageContainers = document.querySelectorAll('.image-container');
+  artistNames = document.querySelectorAll('.artist-name');
+  overlays = document.querySelectorAll('.artist-link-overlay');
+
 });
 
-window.addEventListener('load', (e) =>{
+window.addEventListener('load', function(){
 
-  console.log("Time until everything loaded: ", Date.now()-timerStartLoad);
+  windowLoadTime = Date.now() - timerStartLoad;
 
-  const britt = new Image();
-  const robyn = new Image();
-  const elise = new Image();
-  const cages = new Image();
-  const dogwood = new Image();
-  const fairylights = new Image();
+  console.log("Window Load Time: ", windowLoadTime);
 
-  const artists = [britt, robyn, elise, cages, dogwood, fairylights];
-  const imageContainers = document.querySelectorAll('.image-container');
-  const artistNames = document.querySelectorAll('.artist-name');
-  const overlays = document.querySelectorAll('.artist-link-overlay');
-
-  britt.src = 'images/artists/BRITT_1.jpg';
-  britt.alt = 'BRITT MARGIT';
-
-  robyn.src = 'images/artists/ROBYN_1.jpg';
-  robyn.alt = 'ROBYN SHERWELL';
-
-  elise.src = 'images/artists/ELISE_2.jpg';
-  elise.alt = 'ELISE LEGENDRE';
-
-  cages.src = 'images/artists/CAGES_1.jpg';
-  cages.alt = 'CAGES OF LIGHT';
-
-  dogwood.src = 'images/artists/DOGWOOD_1.jpg';
-  dogwood.alt = 'DOGWOOD HEART';
-
-  fairylights.src = 'images/artists/FAIRYLIGHTS_2.jpg';
-  fairylights.alt = 'THE FAIRYLIGHTS';
-
-  setTimeout(() =>{
+  setTimeout(function(){
 
     imageContainers.forEach((x, i) =>{
       x.appendChild(artists[i])
@@ -53,6 +67,6 @@ window.addEventListener('load', (e) =>{
       x.style.visibility = 'visible'
     });
 
-  }, (Date.now()-timerStartLoad) * 1.5);
+  }, windowLoadTime * 1.5);
 
 });
