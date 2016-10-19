cb7a2af8dc7a45912f6ab750d1cefa1ef31e3a01
MickeyJ <develop.agility@gmail.com>
2016-10-19 13:34:24 -0600

    artist page banner progress

--- routes/artist_data.js
+++ routes/artist_data.js
@@ -1,37 +1,37 @@
 
 module.exports = [
   {
-    name: 'Britt Margit',
+    name: 'BRITT MARGIT',
     website: '',
     img_url: 'images/color/BRITT_color2.jpg',
     path:  'britt_margit'
   },
   {
-    name: 'Robyn Sherwell',
+    name: 'ROBYN SHERWELL',
     website: '',
     img_url: 'images/color/ROBYN_color2.jpg',
     path:  'robyn_sherwell'
   },
   {
-    name: 'Elise Legendre',
+    name: 'ELISE LEGENDRE',
     website: '',
     img_url: 'images/color/ELISE_color2.jpg',
     path:  'elise_legendre'
   },
   {
-    name: 'Cages of Light',
+    name: 'CAGES OF LIGHT',
     website: '',
     img_url: 'images/color/CAGES_color2.jpg',
     path:  'cages_of_light'
   },
   {
-    name: 'Dogwood Heart',
+    name: 'DOGWOOD HEART',
     website: '',
     img_url: 'images/color/DOGWOOD_color2.jpg',
     path:  'dogwood_heart'
   },
   {
-    name: 'The Fairylights',
+    name: 'THE FAIRYLIGHTS',
     website: '',
     img_url: 'images/color/FAIRYLIGHTS_color2.jpg',
     path:  'the_fairylights'
--- views/artist.jsx
+++ views/artist.jsx
@@ -11,9 +11,38 @@ export default class Artist extends Component{
 
         <section id="artist-page-container">
           <div id="artist-page-banner">
-            <img className="artist-page-image" src={artist.img_url} alt={artist.name} />
+
+            <div id="artist-image-container">
+
+              <img className="artist-page-image" src={artist.img_url} alt={artist.name} />
+              <div className="artist-name-overlay">
+                <span>{artist.name}</span>
+              </div>
+
+            </div>
+
+            <div id="artist-links-container">
+
+              <div className="artist-links">
+                <a className="artist-official-link" href={`#${artist.name}`}>
+                  Official Site
+                </a>
+                <div className="artist-social-links">
+                  <a href="#!TW" >
+                    <img src="icons/tw-shadow.png" style={{height: 52, width: 52, marginBottom: -4}}  alt="TW" />
+                  </a>
+                  <a href="#FB" >
+                    <img src="icons/fb-shadow.png" style={{height: 50, width: 50, marginBottom: 0}} alt="FB" />
+                  </a>
+                  <a href="#BL email" >
+                    <img src="icons/mail_2.png" style={{height: 45, width: 45, marginBottom: 2}} alt="BL" />
+                  </a>
+                </div>
+              </div>
+
+            </div>
+
           </div>
-          <h1>{artist.name}</h1>
 
         </section>
 
